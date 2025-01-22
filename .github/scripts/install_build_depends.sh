2b0008a2cf9bb531e3d555f46661128fce399638
Deric Crago <deric.crago@gmail.com>
2025-01-22 16:24:06 -0500

    updates to enable Ansible 11 builds (#100)
    * bumped 'runs-on' to 'ubuntu-24.04'
    
    Ansible 11 requires python 3.11 and ubuntu 22.04 ships with 3.10
    
    * added a virtual environment for the docs build (see error below)
    
    error: externally-managed-environment
    
    × This environment is externally managed
    ╰─> To install Python packages system-wide, try apt install
        python3-xyz, where xyz is the package you are trying to
        install.
    
        If you wish to install a non-Debian-packaged Python package,
        create a virtual environment using python3 -m venv path/to/venv.
        Then use path/to/venv/bin/python and path/to/venv/bin/pip. Make
        sure you have python3-full installed.
    
        If you wish to install a non-Debian packaged Python application,
        it may be easiest to use pipx install xyz, which will manage a
        virtual environment for you. Make sure you have pipx installed.
    
        See /usr/share/doc/python3.12/README.venv for more information.
    
    note: If you believe this is a mistake, please contact your Python
    installation or OS distribution provider. You can override this, at
    the risk of breaking your Python installation or OS, by passing
     --break-system-packages.
    hint: See PEP 668 for the detailed specification.
    
    * added 'pyproject' pybuild plugin
    
    Ansible 11 now uses pyproject exclusively
    
    * added 'build-essential'
    
    resolves 'Unmet build dependencies: build-essential:native' in CI

--- .github/scripts/install_build_depends.sh
+++ .github/scripts/install_build_depends.sh
@@ -3,10 +3,13 @@
 sudo apt-get update
 
 sudo apt-get -y install \
+    build-essential \
     debhelper \
     devscripts \
     dh-python \
+    pybuild-plugin-pyproject \
+    python3 \
     python3-pip \
     python3-setuptools \
-    python3 \
+    python3-venv \
     wget
